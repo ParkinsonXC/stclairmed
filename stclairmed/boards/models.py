24f05a7620913715f9f29c7c650af1c114af29fc
neondodongo <willirmler@me.com>
2018-05-17 14:46:54 -0500

    created/modified doctor, specialty, and practice models

--- stclairmed/boards/models.py
+++ stclairmed/boards/models.py
@@ -11,5 +11,20 @@ class Specialty(models.Model):
 class Practice(models.Model):
     name = models.CharField(max_length=45, unique=True)
     address = models.CharField(max_length = 50, unique=True)
-    number = models.IntegerField()
-    specialty = models.ForeignKey(Specialty, related_name='practices', on_delete='CASCADE')
+    city = models.CharField(max_length=25, default='')
+    state = models.CharField(max_length=25, default='IL')
+    zip_code = models.CharField(max_length=5, default='')
+
+    def __str__(self):
+        return self.name
+
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    number = models.CharField(max_length=10)
+    practice = models.ForeignKey(Practice, related_name='doctors', on_delete='CASCADE')
+    specialty = models.ForeignKey(Specialty, related_name='doctors', on_delete='CASCADE')
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
