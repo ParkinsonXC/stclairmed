10c7001afef097c354bc812e8577345515f3458b
Josh Parkinson <parkinsonxc@gmail.com>
2018-05-17 15:31:37 -0500

    added the doctors to the admin function

--- stclairmed/boards/admin.py
+++ stclairmed/boards/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Specialty, Practice 
+from .models import Specialty, Practice, Doctor
 # Register your models here.
 admin.site.register(Specialty)
 admin.site.register(Practice)
+admin.site.register(Doctor)
