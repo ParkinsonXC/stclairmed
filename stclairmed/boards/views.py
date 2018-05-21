ce5da427aa57af3b7b8ebd0ab0817055a2d189e4
neondodongo <willirmler@me.com>
2018-05-21 16:41:14 -0500

    RSVP form not performing POST

--- stclairmed/boards/views.py
+++ stclairmed/boards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Specialty, Practice, Doctor
 from .forms import PracticeSearchForm, ContactForm
 from events.forms import RsvpForm
-from events.models import Event
+from events.models import Event, RSVP
 
 # Create your views here.
 def home(request):
@@ -28,9 +28,25 @@ def officers(request):
     return render(request, 'officers.html')
 
 def events(request):
-    events = reversed(Event.objects.all())
+    events = reversed(Event.objects.all()) # Show newest event first
     form = RsvpForm()
     return render(request, 'events.html', {'events':events, 'form':form})
+
+def event_rsvp(request, pk):
+    if request.method == 'POST':
+        event = get_object_or_404(Event, pk)
+        form = RsvpForm(request.POST)
+        if form.is_valid():
+            rsvp = RSVP.objects.create(
+                first_name=form.cleaned_data.get('first_name'),
+                last_name=form.cleaned_data.get('last_name'),
+                email=form.cleaned_data.get('email'),
+                guests=form.cleaned_data.get('guests'),
+                event=event
+            )
+            return render(request, 'event_confirm.html', {'email':rsvp.email})
+
+    return redirect('events')
 
 def news(request):
     return render(request, 'news.html')
--- stclairmed/events/events_views.py
+++ stclairmed/events/events_views.py
@@ -1,9 +1,1 @@
 from django.shortcuts import render
-
-from events.models import Event
-from events.forms import RsvpForm
-
-def event_rsvp(request, pk):
-    event = get_object_or_404(Events, pk=pk)
-    form = RsvpForm()
-    return render(request, 'rsvp.html', {'event':event, 'form':form})
