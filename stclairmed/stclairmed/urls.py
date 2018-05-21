69cc45e7f2acfa8d485ddd5b042bd8221b89c678
NynjaOrca <spencer.tyminski@gmail.com>
2018-05-21 18:38:09 -0500

    Added link to NEWSLETTERS and got rid of LINKS link

--- stclairmed/stclairmed/urls.py
+++ stclairmed/stclairmed/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     url(r'^news/$', views.news, name='news'),
     url(r'^newsletter/$', views.newsletter, name='newsletter'),
     url(r'^hospitals/$', views.hospitals, name='hospitals'),
-    url(r'^links/$', views.links, name='links'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^specialty/(?P<pk>\d+)/$', views.spec_description, name="spec_description"),
     url(r'^admin/', admin.site.urls),
