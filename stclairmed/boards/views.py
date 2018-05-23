8bf4930a231a43230a8b3a5ca04995b41dca5078
Josh Parkinson <parkinsonxc@gmail.com>
2018-05-23 11:58:18 -0500

    The contact form is now pulling from the forms.py class instead of being in the html

--- stclairmed/boards/views.py
+++ stclairmed/boards/views.py
@@ -5,9 +5,7 @@ from .forms import SearchForm, ContactForm
 # The import below has been added for newsletter functionality
 from newsletters.models import Newsletter
 from events.models import Event
-from events.forms import RsvpForm
-
-
+from events.forms import RsvpForm, RSVP
 
 
 # Create your views here.
