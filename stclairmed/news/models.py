c8a155a6188296c6ca04c90cb35998bd6fbf1913
NynjaOrca <spencer.tyminski@gmail.com>
2018-05-27 13:48:12 -0500

    News page has been created

---
+++ stclairmed/news/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=100, unique=True, blank=False)
+    body = models.TextField(max_length=4000, blank=False)
+    date = models.DateField(auto_now_add=True, blank=False)
+    time = models.TimeField(auto_now=True, blank=False)
+
+    def __str__(self):
+        return self.title + ' ' + str(self.date)
