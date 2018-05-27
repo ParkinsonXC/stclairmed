d04c945ff96a61e8da1fac051bdca573b83ae80a
NynjaOrca <spencer.tyminski@gmail.com>
2018-05-27 16:29:55 -0500

    Updated for use of img

--- stclairmed/news/models.py
+++ stclairmed/news/models.py
@@ -7,6 +7,7 @@ class Announcement(models.Model):
     body = models.TextField(max_length=4000, blank=False)
     date = models.DateField(auto_now_add=True, blank=False)
     time = models.TimeField(auto_now=True, blank=False)
+    img = models.ImageField(upload_to='media', blank=True)
 
     def __str__(self):
         return self.title + ' ' + str(self.date)
